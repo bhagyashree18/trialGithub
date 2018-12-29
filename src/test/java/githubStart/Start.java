13d656211bb2a828b3d3d2a45dafd167d0347a9d
Bhagyashree18 <bhagyashreemhetre@gmail.com>
2018-12-29 19:11:03 +0530

    First commit

---
+++ src/test/java/githubStart/Start.java
@@ -0,0 +1,12 @@
+package githubStart;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to github");
+		System.out.println("This is a Project");
+
+	}
+
+}
